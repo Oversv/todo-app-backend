c5f099ad83b01168b06d87582cb7a25e8f68791b
Alberto MDV <albertomdv@gmail.com>
2019-11-27 12:32:21 +0000

    conection DB

--- handler.js
+++ handler.js
@@ -2,15 +2,35 @@ const express = require("express");
 const serverlessHttp = require("serverless-http");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const mysql = require("mysql");
+
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get("/tasks", function(request, response){
-  //Do the logic for getting all the tasks from the DB
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: "todo_app"
+});
 
-  response.status(200).send("Getting all the tasks! Yay!");
+app.get("/tasks", function(request, response){
+
+  connection.query("SELECT * FROM task", function(err, data) {
+    if (err) {
+      console.log("Error fetching tasks", err);
+      response.status(500).json({
+        error: err
+      });
+    } else {
+      response.json({
+        tasks: data
+      });
+    }
+  });
+
 });
 
 app.post("/tasks", function(request, response){
